c6d77a0dc6bf41eef7e43f4316bd59f9cbbc6f90
Markus <markus.s.englund@gmail.com>
2018-04-08 00:07:24 +0200

    enable copying css other files from src/lib to lib

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -2,6 +2,7 @@ import React from "react";
 import { render } from "react-dom";
 import MyComponent from "../../lib";
 import "./styles.css";
+import "../lib/styles.css";
 
 function Demo() {
   return (
