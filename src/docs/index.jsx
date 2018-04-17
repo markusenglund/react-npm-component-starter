34f455ff21f9bf0a5145cafb34c0522f06eb6990
Alvian Rahman Hanif <alvian524@gmail.com>
2018-04-17 13:36:55 +0700

    add support for Object Spread

--- src/docs/index.jsx
+++ src/docs/index.jsx
@@ -6,8 +6,9 @@ import "./styles.css";
 function Demo() {
   return (
     <div>
-      <Meta title="vian app">
-        <h1>Demo with examples of the component</h1>
+      <Meta title="Title">
+        <div>
+        </div>
       </Meta>
     </div>
   );
