0b1b105bacd142f9b45be5d2df7f495a05abd027
Markus <markus.s.englund@gmail.com>
2018-04-24 21:44:10 +0200

    add stage-2 support

--- src/lib/index.jsx
+++ src/lib/index.jsx
@@ -1,7 +1,18 @@
 import React, { Component } from "react";
 
-const MySuperCoolComponent = ({ children, color }) => (
-  <button style={{ color }}>{children}</button>
-);
+class MySuperCoolComponent extends Component {
+  handleClick = () => {
+    console.log("Click!");
+  };
+
+  render() {
+    const { color, children } = this.props;
+    return (
+      <button onClick={this.handleClick} style={{ color }}>
+        {children}
+      </button>
+    );
+  }
+}
 
 export default MySuperCoolComponent;
